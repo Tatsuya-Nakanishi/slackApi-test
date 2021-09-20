7ab90425ef3b2aadb4de2dcb395556fdf54cd9c2
Tatsuya Nakanishi <jwtja6wv@gmail.com>
2021-09-20 17:00:15 +0900

    コメント修正

--- backend/app/Http/Controllers/SampleController.php
+++ backend/app/Http/Controllers/SampleController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Http\JsonResponse;
 
 class SampleController extends Controller
 {
@@ -30,47 +28,28 @@ class SampleController extends Controller
             //トークンは.envファイルに設定
             'Authorization'             => 'Bearer ' . env('API_KEY', null),
         ];
+
+        //チャンネルIDと最大取得件数をリクエストパラメータに指定。
         $form_params    = [
-            //チャンネルIDと最大取得件数を指定。
             'channel' => 'C02CQN57B7U',
             'limit' => 1,
         ];
-        // dd($path);
+        
         $response = $client->request('GET', $base_url, [
             'allow_redirects' => true,
             'headers'         => $headers,
             'verify'          => false,
             'query'           => $form_params,
         ]);
+
+        //こちらの記述で日本語の文字化けが直らない
         $response_body =  (string) $response->getBody();
         json_encode($response_body, JSON_UNESCAPED_UNICODE); 
         return $response_body;
 
-        //return $this->jsonResponse($response_body);
-         //dd($response_body);
-         //var_dump(json_encode($response_body));
-        //echo $response_body;
     }
 
-    //新規投稿取得メソッド
-    // public function getNewMessage()
-    // {
-    //     $path           = '/conversations.history';     // 呼び出すSlack Method
-    //     $form_params    = [
-    //         'channel' => 'C02CQN57B7U',
-    //         'limit' => 1,
-    //     ]; 
-    //     $this->callSlackApi($path, $form_params);
-    // }
+    //過去投稿取得メソッド追記予定
 
-    //過去の投稿取得メソッド
-    // public function getPastMessages()
-    // {
-    //     $path           = '/conversations.history';     // 呼び出すSlack Method
-    //     $form_params    = [
-    //         'channel' => 'C02CQN57B7U',
-    //         //期間指定のパラメータを挿入
-    //     ]; 
-    //     $this->callSlackApi($path, $form_params);
-    // }
+
 }
